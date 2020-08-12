461d9a408c129ebae19d8e7fbd68806f200b2abc
wngus6429 <55697824+wngus6429@users.noreply.github.com>
2020-08-12 15:00:00 +0900

    backup

--- server/index.js
+++ server/index.js
@@ -6,14 +6,12 @@ const cookieParser = require("cookie-parser");
 const config = require("./config/key"); //보안처리 불러옴
 const { auth } = require("./middleware/auth");
 const { User } = require("./models/User"); //모델 불러옴
-
 //bodyparser는 클라이언트에서 오는 정보를 서버에서 분석해서 가져올수있게 해줌
 //application/x-www-form- 이런걸 분석해서 가져옴
 app.use(bodyParser.urlencoded({ extended: true }));
 //application/json 가져옴
 app.use(bodyParser.json());
 app.use(cookieParser());
-
 //app.use("/api/users", require("./routes/users"));
 app.use("/api/video", require("./routes/video")); //이게 있어야 routes 파일 접근가능
 app.use("/api/subscribe", require("./routes/subscribe")); //이게 있어야 routes 파일 접근가능
@@ -22,7 +20,6 @@ app.use("/api/like", require("./routes/like"));
 app.use("/uploads", express.static("uploads"));
 //이거 있어야함, 이미지, CSS 파일 및 JavaScript 파일과 같은 정적 파일을 제공하려면
 //Express의 기본 제공 미들웨어 함수인 express.static을 사용하십시오.
-
 //mongoose는 MongoDb 연결 담당임, 뒤에 {useNew, Unified, useCre 등등 적어줘야 에러 안남}
 //.then()은 앞에 부분이 실행이 끝나면 호출됨. catch로 에러 잡기
 //////////////////////////////////////////////////////////////////////////////////////////////
@@ -37,23 +34,21 @@ mongoose
   .then(() => console.log("MongoDB connect Success"))
   .catch((error) => console.log(error));
 /////////////////////////////////////////////////////////////////////////////////////////////
+const port = 5000;
+app.listen(port, () => console.log(`Connection Success Click = ( http://localhost:3000 )`));
+/////////////////////////////////////////////////////////////////////////////////////////////
 
 app.get("/", (req, res) => {
-  res.send("Hello World Park Juhyun さま");
-});
-
-app.get("/api/hello", (req, res) => {
-  res.send("안녕하세요~~");
+  res.send("여기는 서버 반응, http://localhost:3000 로 가세요");
 });
 
 app.post("/api/users/register", (req, res) => {
   const user = new User(req.body);
-  //밑에 이건 몽고DB에서 오는 메소드
   user.save((err, userInfo) => {
+    //이건 몽고DB에서 오는 메소드
     if (err) return res.json({ success: false, err }); //에러가 있으면 전달, 뒤 err가 에러메세지 전달 내용
     return res.status(200).json({ success: true, userInfo }); //status(200)이건 성공했다는 거임
-  });
-  //회원가입할때 필요한 정보들은 client에서 가져오면 그것들은 데이터베이스에 넣어준다.
+  }); //회원가입할때 필요한 정보들은 client에서 가져오면 그것들은 데이터베이스에 넣어준다.
 });
 
 app.post("/api/users/login", (req, res) => {
@@ -61,10 +56,7 @@ app.post("/api/users/login", (req, res) => {
   User.findOne({ email: req.body.email }, (err, user) => {
     if (!user) {
       //해당하는 유저가 없을경우
-      return res.json({
-        loginSuccess: false,
-        message: "This Email not Correct",
-      });
+      return res.json({ loginSuccess: false, message: "This Email not Correct" });
     }
     //요청한 이메일이 DB에 있으면 비밀번호가 맞는 비밀번호 인지 확인, compare이건 이름 자유임. 단 User.js도 바꾸기
     user.comparePassword(req.body.password, (err, isMatch) => {
@@ -83,7 +75,6 @@ app.post("/api/users/login", (req, res) => {
 
 app.get("/api/users/auth", auth, (req, res) => {
   //중간 auth 에서 로그인한 유저인지 판별 들웨어를 통과해 왔다는 이야기는 Authentication 이 True라는 말.
-  //
   res.status(200).json({
     _id: req.user._id, // 이게 가능한 이유는 Auth.js 15에서 했기 때문
     isAdmin: req.user.role === 0 ? false : true, //Role 1 어드민, ROle 2 특정부서어드민, Role 0 일반유저
@@ -100,12 +91,6 @@ app.get("/api/users/logout", auth, (req, res) => {
   // console.log('req.user', req.user)
   User.findOneAndUpdate({ _id: req.user._id }, { token: "", tokenExp: "" }, (err, user) => {
     if (err) return res.json({ success: false, err });
-    return res.status(200).send({
-      success: true,
-    });
+    return res.status(200).send({ success: true });
   });
 });
-
-const port = 5000;
-
-app.listen(port, () => console.log(`Connection Success Click = ( http://localhost:${port} )`));
