efc7d3a3d98a5f2696993a5cf2b2df83be026dce
wngus6429 <55697824+wngus6429@users.noreply.github.com>
2020-08-19 01:34:25 +0900

    heroku deploy

--- server/config/prod.js
+++ server/config/prod.js
@@ -1,6 +1,6 @@
 /** @format */
 
 module.exports = {
-  MONGOURL: process.env.MONGO_URI,
+  MONGOURL: process.env.MONGOURL,
 };
 //heroku에서 하는거임. 뒤의 MONGO_URI가 일치해야함.
--- server/index.js
+++ server/index.js
@@ -34,7 +34,17 @@ mongoose
   .then(() => console.log("MongoDB connect Success"))
   .catch((error) => console.log(error));
 /////////////////////////////////////////////////////////////////////////////////////////////
-const port = 5000;
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  //set static folder
+  //All the javascript and css files will be read and served from this folder
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+  });
+}
+
+const port = process.env.PORT || 5000;
+
 app.listen(port, () =>
   console.log(
     `Connection Success Click = (React/ http://localhost:3000 )(Server/ http://localhost:5000)`
